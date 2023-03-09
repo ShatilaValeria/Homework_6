4497f311d68124276c43129574bebc7e99d88387
ValeriaShatila <123481670+ValeriaShatila@users.noreply.github.com>
2023-03-09 15:03:54 +0300

    Changes MainClass

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -9,26 +9,12 @@ public class MainClass {
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://onliner.by");
         driver.findElement(By.xpath("//span[text()='Фены']")).click();
-        ((JavascriptExecutor)driver).executeScript("window.scrollBy(0, 250)");
-        driver.findElement(By.xpath("//input[@value='dyson']/following-sibling::span")).click();
+        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 250)");
+        driver.findElement(By.xpath("//input[@value='philips']/following-sibling::span")).click();
 
-        if (driver.findElement(By.xpath("//input[@value='dyson']")).isSelected()) {
+        if (driver.findElement(By.xpath("//input[@value='philips']")).isSelected()) {
             System.out.println("Still selected");
         }
         driver.quit();
-
-//        System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
-//     //   ChromeDriver driver = new ChromeDriver();
-//        driver.get("https://onliner.by");
-//        driver.findElement(By.xpath("//span[text()='Фены']")).click();
-//        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 250)");
-//        driver.findElement(By.xpath("//input[@value='dyson']/following-sibling::span")).click();
-//
-//        if (driver.findElement(By.xpath("//input[@value='philips']")).isSelected()) {
-//            System.out.println("Вывелись верные позиции");
-//        }
-//
-//        driver.quit();
-
     }
 }
